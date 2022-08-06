91de44c1891d139411665a6e698e6c94a4983641
Saiprasad Subhash Patil <110680594+Saiprasad1010@users.noreply.github.com>
2022-08-06 17:38:32 +0530

    Update motorcycle.java

--- src/interfacedemo/motorcycle.java
+++ src/interfacedemo/motorcycle.java
@@ -4,6 +4,7 @@ public interface motorcycle {
 	void motorcyclerun();
 	void motorcyclewheel();
 	void motorcycleprice();
+	void motorcycleprices1();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
